541cb1530ff711c62cc54794baf9d120d526eeff
dungnv <dungnv@nal.vn>
2014-09-25 17:47:23 +0700

    Add migration

---
+++ GoiChe/app/database/migrations/2014_09_25_180007_create_order_session_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrderSessionTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('order_session', function(Blueprint $table)
+    {
+      $table->increments('id');
+      $table->timestamp('date');
+      $table->timestamp('start');
+      $table->timestamp('end');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('order_session');
+  }
+
+}
