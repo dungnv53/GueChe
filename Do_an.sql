69c9ddad0a177aeec986d16596390dbb356a711d
dungnv <dungnv@nal.vn>
2014-09-25 14:31:38 +0700

    First kick

---
+++ Do_an.sql
@@ -0,0 +1,12 @@
+1. Catelog:
+  - eg. Món ăn, sữa chua, chè, đồ uống, hoa quả dầm ...
+2. Food:
+  - Tên các món cụ thể theo danh mục:
+  Món ăn: có nem, bánh ...
+  Chè: các loại như đậu đỏ, mít, bưởi ...
+
+
+
+3. User
+4. Admin
+5. Cart
